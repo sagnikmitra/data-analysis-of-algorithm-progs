7f8801767a07dc14d9794900d0a3a2b1b91fca25
Shubham Tribedi <shubhamtribedi89@gmail.com>
2020-12-11 21:28:30 +0530

    Update min_max_dc.cpp

--- min_max_dc.cpp
+++ min_max_dc.cpp
@@ -1,71 +1,55 @@
-#include <iostream>
-#include <climits>
-using namespace std;
- 
-// Divide & Conquer solution to find minimum and maximum number in an array
-void findMinAndMax(int arr[], int low, int high, int& min, int& max)
+#include<stdio.h>
+#include<stdio.h>
+int max, min;
+int a[100];
+void maxmin(int i, int j)
 {
-    // if array contains only one element
- 
-    if (low == high)            // common comparison
-    {
-        if (max < arr[low])     // comparison 1
-            max = arr[low];
- 
-        if (min > arr[high])    // comparison 2
-            min = arr[high];
- 
-        return;
-    }
- 
-    // if array contains only two elements
- 
-    if (high - low == 1)            // common comparison
-    {
-        if (arr[low] < arr[high])    // comparison 1
-        {
-            if (min > arr[low])     // comparison 2
-                min = arr[low];
- 
-            if (max < arr[high])    // comparison 3
-                max = arr[high];
-        }
-        else
-        {
-            if (min > arr[high])    // comparison 2
-                min = arr[high];
- 
-            if (max < arr[low])     // comparison 3
-                max = arr[low];
-        }
-        return;
-    }
- 
-    // find mid element
-    int mid = (low + high) / 2;
- 
-    // recur for left sub-array
-    findMinAndMax(arr, low, mid, min, max);
- 
-    // recur for right sub-array
-    findMinAndMax(arr, mid + 1, high, min, max);
+ int max1, min1, mid;
+ if(i==j)
+ {
+  max = min = a[i];
+ }
+ else
+ {
+  if(i == j-1)
+  {
+   if(a[i] <a[j])
+   {
+    max = a[j];
+    min = a[i];
+   }
+   else
+   {
+    max = a[i];
+    min = a[j];
+   }
+  }
+  else
+  {
+   mid = (i+j)/2;
+   maxmin(i, mid);
+   max1 = max; min1 = min;
+   maxmin(mid+1, j);
+   if(max <max1)
+    max = max1;
+   if(min > min1)
+    min = min1;
+  }
+ }
 }
- 
-int main()
+int main ()
 {
-    int arr[] = { 7, 2, 9, 3, 1, 6, 7, 8, 4 };
-    int n = sizeof(arr) / sizeof(arr[0]);
- 
-    // initialize the minimum element by infinity and the
-    // maximum element by minus infinity
-    int max = INT_MIN, min = INT_MAX;
- 
-    findMinAndMax(arr, 0, n - 1, min, max);
- 
-    cout << "The minimum element in the array is " << min << '\n';
-    cout << "The maximum element in the array is " << max;
- 
-    return 0;
+ int i, num;
+ printf ("\nEnter the total number of numbers : ");
+ scanf ("%d",&num);
+ printf ("Enter the numbers : \n");
+ for (i=1;i<=num;i++)
+  scanf ("%d",&a[i]);
+
+ max = a[0];
+ min = a[0];
+ maxmin(1, num);
+ printf ("Minimum element in an array : %d\n", min);
+ printf ("Maximum element in an array : %d\n", max);
+ return 0;
 }
-
-
